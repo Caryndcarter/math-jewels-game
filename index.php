c6761918ccbdc7762d8e85a975bd4cd4b7e012c1
Caryn Carter <carter.caryn@gmail.com>
2020-02-04 09:53:43 -0600

    added explanation of why there is a php file

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php include_once("index.html"); ?>
+<?php include_once("index.html"); 
+
+//this file is only here because it will make Heroku treat the app as a PHP app and that is the easiest kind to set up
+
+?>
